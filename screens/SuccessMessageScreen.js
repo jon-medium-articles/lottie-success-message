7bad3d389a062c0f83ea0e6d3b8de1906ea03a94
Jon <jonoyanguren@gmail.com>
2022-09-12 08:07:28 +0200

    final

--- screens/SuccessMessageScreen.js
+++ screens/SuccessMessageScreen.js
@@ -1,9 +1,24 @@
 import { View, Text, StyleSheet } from "react-native";
+import Lottie from "lottie-react-native";
 
 export const SuccessMessageScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Success!</Text>
+      <View style={styles.lottie}>
+        <Lottie
+          source={require("../assets/animations/success.json")}
+          autoPlay
+          loop={false}
+        />
+      </View>
+
+      <View>
+        <Text style={styles.title}>Great job!</Text>
+        <Text style={styles.text}>
+          You have animated one screen, now your UI quality deliverables has
+          increased.
+        </Text>
+      </View>
     </View>
   );
 };
@@ -13,8 +28,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  lottie: {
+    width: 200,
+    height: 200,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: "center",
+    marginTop: 20,
   },
   text: {
-    fontSize: 30,
+    fontSize: 18,
+    marginTop: 10,
+    color: "#757575",
+    textAlign: "center",
   },
 });
